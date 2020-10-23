a8864bb9209b949dd2a00736ff283ffe74cfc2e0
Peter <picusbrit@gmail.com>
2020-10-23 17:32:09 -0400

    Converted Basic A* custom implementation to Unity NavMesh.

--- Assets/FollowPath.cs
+++ Assets/FollowPath.cs
@@ -1,33 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowPath : MonoBehaviour
 {
-    Transform goal;
-    float speed = 5.0f;
-    public float accuracy = 1.0f;
-    public float rotSpeed = 2.0f;
+
     public GameObject wpManager;
     GameObject[] wps;
-    GameObject currentNode;
-    int currentWP = 0;
-    Graph g;
+    NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         wps = wpManager.GetComponent<WPManager>().waypoints;
-        g = wpManager.GetComponent<WPManager>().graph;
-        currentNode = wps[4];
+        agent = GetComponent<NavMeshAgent>();
     }
     
     public void GoToHeli()
     {
         if (wps[1])
         {
-            g.AStar(currentNode, wps[1]);
-            currentWP = 0;
+            agent.SetDestination(wps[1].transform.position);
         }
         else Debug.Log("There is nothing at waypoint 1");
     }
@@ -36,8 +30,7 @@ public class FollowPath : MonoBehaviour
     {
         if (wps[8])
         {
-            g.AStar(currentNode, wps[8]);
-            currentWP = 0;
+            agent.SetDestination(wps[8].transform.position);
         }
         else Debug.Log("There is nothing at waypoint 8");
     }
@@ -46,9 +39,7 @@ public class FollowPath : MonoBehaviour
     {
         if (wps[15])
         {
-            g.AStar(currentNode, wps[15]);
-
-            currentWP = 0;
+            agent.SetDestination(wps[15].transform.position);
         }
         else Debug.Log("There is nothing at waypoint 15");
     }
@@ -57,38 +48,12 @@ public class FollowPath : MonoBehaviour
     {
         if (wps[19])
         {
-            g.AStar(currentNode, wps[19]);
-
-            currentWP = 0;
+            agent.SetDestination(wps[19].transform.position);
         }
         else Debug.Log("There is nothing at waypoint 19");
     }
         void LateUpdate()
     {
-        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
-        {
-            return;
-        }
-        
-        //the node we are closest to at this moment
-        currentNode = g.getPathPoint(currentWP);
 
-        //if we are close enough to the current waypoint move to the next
-        if(Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position)<accuracy)
-        {
-            currentWP++;
-        }
-
-        //if we are not at the end of the path
-        if (currentWP < g.getPathLength())
-        {
-            goal = g.getPathPoint(currentWP).transform;
-            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
-            Vector3 direction = lookAtGoal - this.transform.position;
-            //Rotate
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
-            //Move
-            this.transform.Translate(0, 0, speed * Time.deltaTime);
-        }
     }
 }
